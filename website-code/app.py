d628ed32291e42e4a973dae8ca1b62c7f7fc8516
Ian Roberts <iar31.roberts@gmail.com>
2021-06-02 18:26:31 +0100

    add db details
    /1.1

--- website-code/app.py
+++ website-code/app.py
@@ -1,6 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, flash
+from flask.helpers import url_for
+from werkzeug.utils import redirect
+from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(days=5)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config["SWLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
 
 
 @app.route('/')
@@ -48,14 +68,38 @@ def register():
     return render_template('register.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=["POST", "GET"])
 def login():
-    return render_template('login.html')
+    if request.method == "POST":
+        session.permanent - True
+        user = request.form["username"]
+        session["username"] = user
+
+        found_user = users.query.filter_by(username=user).first()
+        if found_user:
+            session["username"] = found_user.username
+        else:
+            usr = users(user, "")
+            db.session.add(usr)
+            db.session.comit()
+
+        flash("Login Succesful!")
+        return redirect(url_for("/"))
+    else:
+        if "user" in session:
+            flash("Already Logged in")
+            return redirect(url_for("/"))
+        return render_template('login.html')
 
 
 @app.route('/logout')
 def logout():
     return render_template('logout.html')
+
+
+@app.route("/db")
+def database():
+    return render_template("db.html", values=users.query.all())
 
 
 if __name__ == '__main__':
