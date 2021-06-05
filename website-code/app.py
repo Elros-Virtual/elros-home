307df038624ae3e90d38c14efff0729a69c250d0
Ian Roberts <iar31.roberts@gmail.com>
2021-06-05 15:35:23 +0100

    Update login procedure
    /1.1

--- website-code/app.py
+++ website-code/app.py
@@ -63,38 +63,54 @@ def consulting():
     return render_template('consulting.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=["POST", "GET"])
 def login():
-    return render_template('login.html')
-
-
-@app.route('/register', methods=["POST", "GET"])
-def register():
     if request.method == "POST":
-        session.permanent - True
-        user = request.form["username"]
-        session["username"] = user
-
-        found_user = users.query.filter_by(username=user).first()
-        if found_user:
-            session["username"] = found_user.username
-        else:
-            usr = users(user, "")
-            db.session.add(usr)
-            db.session.comit()
-
+        session.permanent = True
+        user = request.form["user"]
+        session["user"] = user
         flash("Login Succesful!")
-        return redirect(url_for("/"))
+        return redirect(url_for("user"))
     else:
         if "user" in session:
-            flash("Already Logged in")
-            return redirect(url_for("login"))
-        return render_template('register.html')
+            flash("Already logged in")
+            return redirect(url_for("user"))
+        return render_template('login.html')
+
+
+# @app.route('/register', methods=["POST", "GET"])
+# def register():
+#     if request.method == "POST":
+#         user = request.form["user"]
+#         session["user"] = user
+#         return redirect(url_for(login))
+#     else:
+#         return render_template('register.html')
+
+
+@app.route('/user', methods=["POST", "GET"])
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("login"))
+
+
+@app.route('/myaccount')
+def myaccount():
+    if "user" in session:
+        user = session["user"]
+        return render_template('myaccount.html')
+    else:
+        return redirect(url_for("login"))
 
 
 @app.route('/logout')
 def logout():
-    return render_template('logout.html')
+    flash("You have been logged out!", "info")
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 
 @app.route("/db")
