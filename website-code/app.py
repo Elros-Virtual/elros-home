6d3a7b1e9411df1572eb66fd0c3ee3fe6bb4d42f
Ian Roberts <iar31.roberts@gmail.com>
2021-06-17 15:41:40 +0100

    Updates emails and images

--- website-code/app.py
+++ website-code/app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sendgrid import SendGrid
 
 app = Flask(__name__)
+app.config['SENDGRID_API_KEY'] = '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'
+app.config['SENDGRID_DEFAULT_FROM'] = 'hello@elrosvirtual.co.uk'
+mail = SendGrid(app)
 
 
-@app.route('/')
+@app.route('/', methods=["POST", "GET"])
 def index():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        message = request.form.get("message")
+
+        mail.send_email(to_email=email,
+                        subject='Thank you for your Contact', text=message)
+
+        return render_template('index.html')
+
+    # else:
     return render_template('index.html')
 
 
@@ -49,5 +64,5 @@ def consulting():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-    # app.run(debug=True)
+    #app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
