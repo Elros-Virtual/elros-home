18ee827df674bcb1a210899fbf5e22afedc2a1d1
Ian Roberts <iar31.roberts@gmail.com>
2021-07-06 17:02:32 +0100

    update new banner to site

--- website-code/app.py
+++ website-code/app.py
@@ -64,5 +64,5 @@ def consulting():
 
 
 if __name__ == '__main__':
-    #app.run(debug=True, host='0.0.0.0')
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
+    # app.run(debug=True)
